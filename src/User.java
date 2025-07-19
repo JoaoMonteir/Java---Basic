405d3c278d1a9b236a5c6ce571f414316f645406
Joao Monteiro <joaomonteiro.vm@gmail.com>
2025-07-19 18:34:14 -0300

    atualização

---
+++ src/User.java
@@ -0,0 +1,17 @@
+abstract class User {
+
+    String name;
+
+    void greeting() {
+        System.out.println("Olá, " + name + "!");
+    }
+
+    abstract void confirm(); // sem corpo
+}
+
+class Client extends User {
+    @Override
+    void confirm(){
+        System.out.println("Cadastro confirmado.");
+    }
+}
